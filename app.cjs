d486570a132dbe9f41ea7bd7b39e2348f6c15d05
prasadkaru <133613185+prasadkaru@users.noreply.github.com>
2023-11-28 11:31:42 +0530

    Node_js weather app using API

--- app.cjs
+++ app.cjs
@@ -2,6 +2,28 @@
 
 const weather = require('./utils/weather')
 const geo = require('./utils/geo')
+const express  = require('express');
+const app = express();
+
+app.get("/",(req, res)=>{
+    res.send('hello from express app.cjs');
+});
+
+app.get('/about',(req, res)=>{
+    res.send('about')
+});
+
+app.get('/help',(req, res)=>{
+    res.send("from help app.cjs")
+})
+
+app.get('/weather',(req, res)=>{
+    res.send("weather")
+})
+
+app.get('*',(req, res)=>{
+    res.send("page not found")
+})
 // weather(13,-94)
 //     .then((data)=>{
 //         console.log(data);
@@ -10,13 +32,15 @@ const geo = require('./utils/geo')
 //         console.log(error)
 //     })
 
-geo('Colombo')
-    .then((data)=>{
-        return weather(data.lat,data.lon)
-    })
-    .then((data)=>{
-        console.log(data);
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+// geo('Colombo')
+//     .then((data)=>{
+//         return weather(data.lat,data.lon)
+//     })
+//     .then((data)=>{
+//         console.log(data);
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+app.listen(3000,()=>{
+    console.log("server open 3000")});
